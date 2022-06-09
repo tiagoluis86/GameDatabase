9160935b4839c9980d7ed06dfbe4ef1f11e87c9e
Tiago <70555750+tiagoluis86@users.noreply.github.com>
2022-06-09 10:23:04 -0400

    FIX identitation

--- main.py
+++ main.py
@@ -19,16 +19,22 @@ while True:
 
     if op == 1:
         print('Cadastrar Game \n')
-        nomeGame = input('Nome do game: ')
         try:
-		notaGame = float(input('Nota do game (0.0 à 10): '))
-		if 0 <= notaGame <= 10:
-			cadastrarGame(arquivo, nomeGame, notaGame)
-			print("Game {} cadastrado com a nota {} \n".format(nomeGame, notaGame))
-		else:
-			print("Nota inválida. Valor deve ser entre 0 e 10")
-	except:
-		print("Dados inválidos. Verifique se digitou no formato decimal com pontos e não vírgulas")	
+            nomeGame = input('Nome do game: ')
+            notaGame = float(input('Nota do game (0.0 à 10): '))
+            if 0 <= notaGame <= 10:
+                cadastrarGame(arquivo, nomeGame, notaGame)
+                print("Game {} cadastrado com a nota {} \n".format(nomeGame, notaGame))
+                
+            else:
+                print("Nota inválida. Valor deve ser entre 0 e 10")
+                
+        except:
+            print("Dados inválidos. Verifique se digitou no formato decimal com pontos e não vírgulas")
+
+        
+
+
     elif op == 2:
         print('Listar itens \n')
         listarArquivo(arquivo)
